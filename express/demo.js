004bcf5ee44a5157f51f6bc8644178e028fb7457
ryanechternacht <ryan@echternacht.org>
2016-08-07 21:03:20 -0400

    Cleanslate (#2)
    * building a foundation
    
    * Fixed button not working (and moved to state1)
    
    * More stuff
    
    * added webpack write to readme
    
    * Updating readme to include running instructions

---
+++ express/demo.js
@@ -0,0 +1,12 @@
+module.exports = function() {
+
+    var http = require('http');
+
+    return {
+        test: test
+    };
+
+    function test(req, res) {
+        res.send("some information");
+    }
+}
